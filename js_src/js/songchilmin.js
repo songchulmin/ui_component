d51616cfc70fa9d24b4ae66d1a3fe01efd99db2a
songchulmin <rednadasea@gmail.com>
2017-09-07 18:19:12 +0900

    '''

---
+++ js_src/js/songchilmin.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Administrator on 2017-08-29.
+ */
+
+$
+(function() {
+
+  var number = [1, 2, 3, 4, 5];
+
+  for(var i=0; i<5; i++){
+
+    number = number + 1;
+  }
+
+
+  console.log();
+
+});
+
